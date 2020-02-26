a93d0ffae614fbec405dcddff08eb042948aadce
iamnfinity <aman123vishwa@gmail.com>
2020-02-26 22:19:07 +0530

    Added Static Files Added Partials

--- app.js
+++ app.js
@@ -1,28 +1,49 @@
 /**
  * All The Base Imports
  */
-const app = require('express')();
+const express = require('express');
 const body_parser = require('body-parser');
 const morgan = require('morgan');
 
 
 /**
- * Some Important Variables
+ * App Object Declaration
  */
 
+ const app = express();
+
+/**
+ * Some Important Variables
+ */
  const PORT = 8080;
 
 /**
  * Configuring Middlewares 
  */
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms"));
+app.use(body_parser());
 
+
+/**
+ * Express To Serve Static Files
+ */
+ app.use(express.static('public'));
+
+
+ /**
+  * Set Render Engine
+  */
+
+  app.set('view engine','ejs');
+  // Now we can use res.render to render ejs pages
 
 /**
  * Configuring Base Routes
  */
 app.get("/",(req,res)=>{
-    res.send("Server Working Like A Charm");
+    res.render('index',{
+        pageName : 'ColLancer'
+    });
 });
 
 
@@ -31,5 +52,5 @@ app.get("/",(req,res)=>{
  */
 
  app.listen(PORT, ()=>{
-    console.log("Server Working On localhost:"+PORT);~
+    console.log("Server Working On localhost:"+PORT);
  });
